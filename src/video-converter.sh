5c6be20671a181e240d07f94e9e4abfe87f6f9cc
Pavel Stárek <pstarek@gmail.com>
2014-10-11 22:39:22 +0200

    Added some comments and small fixes

--- src/video-converter.sh
+++ src/video-converter.sh
@@ -5,13 +5,14 @@ progname=$(basename $0)
 videobitrate=1228800
 
 # for 16:9 TV capturing
-#aspect=16:9
-#ssize=640x368
+# aspect=16:9
+# ssize=640x368
 
 # for 4:3 TV capturing
-#aspect=4:3
-#ssize=630x504
+# aspect=4:3
+# ssize=630x504
 
+# If none command line arguments are given, or if -h is given as first argument, show some help
 if [[ $1 == "" || $1 == "-h" ]]; then
   echo "Usage:"
   echo "$progname <file.m2t> [aspect_ratio]"
@@ -20,6 +21,7 @@ if [[ $1 == "" || $1 == "-h" ]]; then
   echo "If aspect_ratio parameter is not set, then aspect ratio will be autodetected from input stream."
   exit 0
 fi
+
 
 if [[ $2 == "16" || $2 == "16:9" ]]; then
   aspect=16:9
@@ -37,7 +39,7 @@ if [[ $2 == "4" || $2 == "4:3" ]]; then
   printf "\n"
 fi
 
-filename=$1
+filename="$1"
 
 if [ ! $2 ]; then
   printf "\n"
@@ -57,6 +59,7 @@ if [ ! $2 ]; then
 fi
 
 filenamewithoutext="${filename%%.*}"
+
 mpgext=".mpg"
 aviext=".avi"
 mpgout="$filenamewithoutext$mpgext"
@@ -66,13 +69,14 @@ if [ ! -f "$mpgout" ]; then
   printf "\n"
   ffmpeg -y -i "$filename" -vcodec copy -acodec copy "$mpgout"
   sync
+  printf "\n"
 fi
+
 if [ -f "$mpgout" ]; then
   printf "\n"
   echo "Stage 2 (to AVI), processing $mpgout to $aviout"
   printf "\n"
   ffmpeg -y -i "$mpgout" -vtag DIVX -f avi -vcodec mpeg4 -aspect $aspect -s $ssize -b:v $videobitrate -acodec libmp3lame -b:a 128000 -ar 44100 -ac 2 "$aviout"
+  printf "\n"
 fi
-
-
 
