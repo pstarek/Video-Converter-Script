fdd55d6be1aa3b0dd389faf355017fe8cd3f9524
Pavel Stárek <pstarek@gmail.com>
2017-02-06 19:40:54 +0100

    Updated to use ffmpeg stdin/stdout pipes, so conversion is done by single step

--- src/video-converter.sh
+++ src/video-converter.sh
@@ -2,15 +2,15 @@
 
 #progname=`basename $0`
 progname=$(basename $0)
-videobitrate=1228800
+videobitrate=1331200
 
 # for 16:9 TV capturing
 # aspect=16:9
-# ssize=640x368
+# ssize=640x360
 
 # for 4:3 TV capturing
 # aspect=4:3
-# ssize=630x504
+# ssize=640x480
 
 # If none command line arguments are given, or if -h is given as first argument, show some help
 if [[ $1 == "" || $1 == "-h" ]]; then
@@ -25,7 +25,7 @@ fi
 
 if [[ $2 == "16" || $2 == "16:9" ]]; then
   aspect=16:9
-  ssize=640x368
+  ssize=640x360
   printf "\n"
   echo "16:9 mode"
   printf "\n"
@@ -33,7 +33,7 @@ fi
 
 if [[ $2 == "4" || $2 == "4:3" ]]; then
   aspect=4:3
-  ssize=630x504
+  ssize=640x480
   printf "\n"
   echo "4:3 mode"
   printf "\n"
@@ -64,19 +64,22 @@ mpgext=".mpg"
 aviext=".avi"
 mpgout="$filenamewithoutext$mpgext"
 aviout="$filenamewithoutext$aviext"
-if [ ! -f "$mpgout" ]; then
-  echo "Stage 1 (to MPG), processing file: $filename to $mpgout"
-  printf "\n"
-  ffmpeg -y -i "$filename" -aspect $aspect -target pal-dvd -vcodec copy -acodec copy "$mpgout"
-  sync
-  printf "\n"
-fi
+#if [ ! -f "$mpgout" ]; then
+#  echo "Stage 1 (to PAL-DVD MPG), processing file: $filename to $mpgout"
+#  printf "\n"
+  #ffmpeg -y -i "$filename" -target pal-dvd -g 15 -b 6000000 -maxrate 9000000 -minrate 0 -bufsize 1835008 -packetsize 2048 -muxrate 10080000 -b:a 448000 -aspect $aspect -s 720x576 -b 5000k -b:a 320k -y -mbd rd -trellis 2 -cmp 2 -subcmp 2 -threads 2 "$mpgout"
+#  ffmpeg -y -i "$filename" -aspect $aspect -target pal-dvd -vcodec copy -acodec copy "$mpgout"
+#  sync
+#  printf "\n"
+#fi
 
-if [ -f "$mpgout" ]; then
-  printf "\n"
-  echo "Stage 2 (to AVI), processing $mpgout to $aviout"
-  printf "\n"
-  ffmpeg -y -i "$mpgout" -vtag DIVX -f avi -vcodec mpeg4 -aspect $aspect -s $ssize -b:v $videobitrate -acodec libmp3lame -b:a 128000 -ar 44100 -ac 2 "$aviout"
-  printf "\n"
-fi
+#if [ -f "$mpgout" ]; then
+#  printf "\n"
+#  echo "Stage 2 (to AVI), processing $mpgout to $aviout"
+#  printf "\n"
+#  ffmpeg -y -i "$mpgout" -vtag DIVX -f avi -vcodec mpeg4 -aspect $aspect -s $ssize -b:v $videobitrate -acodec libmp3lame -b:a 128000 -ar 44100 -ac 2 "$aviout"
+#  printf "\n"
+#fi
+
+ffmpeg -y -i "$filename" -aspect $aspect -target pal-dvd -vcodec copy -acodec copy - | ffmpeg -y -i - -vtag DIVX -f avi -vcodec mpeg4 -aspect $aspect -s $ssize -b:v $videobitrate -acodec libmp3lame -b:a 128000 -ar 44100 -ac 2 "$aviout"
 
